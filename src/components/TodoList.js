fe88d66770cf668df3607c4852226bff7f65de53
Apolos666 <quangprox121@gmail.com>
2022-03-27 13:16:43 +0700

    video #7 useState Hook

--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -1,8 +1,13 @@
+import React, { useState } from "react"
 
 const TodoList = () => {
+    const [name, setName] = useState('Quang')
     return (
         <div>
-            Xin chao
+            <label>Name </label>
+            <input value={name} type='text' onChange={(event) => setName(event.target.value)}></input>
+            <br></br>
+            Hello TodoList with name = {name}
         </div>
     )
 }
