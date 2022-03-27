6c4458c54db0b672ad6670b7d6cb424ef8c92fc6
Apolos666 <quangprox121@gmail.com>
2022-03-27 23:27:38 +0700

    video #8 DOM Event in React

--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -2,10 +2,21 @@ import React, { useState } from "react"
 
 const TodoList = () => {
     const [name, setName] = useState('Quang')
+    const handleClickBtn = () => {
+        alert(name)
+    }
     return (
         <div>
             <label>Name </label>
-            <input value={name} type='text' onChange={(event) => setName(event.target.value)}></input>
+            <input
+                value={name}
+                type='text'
+                onChange={(event) => setName(event.target.value)}
+            ></input>
+            <button
+                type='button'
+                onClick={() => handleClickBtn()}
+            >Submit</button>
             <br></br>
             Hello TodoList with name = {name}
         </div>
